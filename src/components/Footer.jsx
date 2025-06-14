18eaaf855474b7e1691157a8b12a005db43e5ca0
Shlok <22cs086@charusat.edu.in>
2025-06-14 15:15:39 +0530

    photos fashion

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import logo1 from "../assets/logo1.png";
 
 export default function Footer() {
   return (
-    <footer className="text-white px-4 pt-0 pb-10">
+    <footer className="text-white px-4 pt-0 pb-4">
       <div className="border-t border-gray-300 bg-black">
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 px-4 py-10">
           {/* Left Side: Contact + Social */}
@@ -36,13 +36,14 @@ export default function Footer() {
           </div>
 
           {/* Right Side: Logo */}
-          <div className="flex justify-end items-center">
-            <img
-              src={logo1}
-              alt="Footer Logo"
-              className="h-28 md:h-32 lg:h-36"
-            />
-          </div>
+        {/* Right Side: Logo */}
+        <div className="flex justify-center md:justify-end items-center">
+          <img
+            src={logo1}
+            alt="Footer Logo"
+            className="h-20 sm:h-28 md:h-32 lg:h-36"
+          />
+        </div>
         </div>
 
         <div className="text-center text-xs font-semibold xl:text-xl md:text-sm text-gray-400 pb-4">
